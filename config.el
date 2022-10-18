64caf83d804f04ffc5519990b24518e49f882f26
Andre Rosendo <andre.rosendo@hotmail.com>
2022-10-18 23:59:10 +0100

    Added some packages
     Changes to be committed:
    	modified:   config.el
    	modified:   init.el

--- config.el
+++ config.el
@@ -98,3 +98,26 @@
 ;; PLANTUML-MODE configurations
 ;; execute plantuml from jar and not from the website
 (setq plantuml-default-exec-mode 'jar)
+
+
+;; spelling configurations
+(with-eval-after-load "ispell"
+  ;; Configure `LANG`, otherwise ispell.el cannot find a 'default
+  ;; dictionary' even though multiple dictionaries will be configured
+  ;; in next line.
+  (setenv "LANG" "en_US.UTF-8")
+  (setq ispell-program-name "hunspell")
+  ;; Configure German, Swiss German, and two variants of English.
+  (setq ispell-dictionary "pt_PT,en_US")
+  ;; ispell-set-spellchecker-params has to be called
+  ;; before ispell-hunspell-add-multi-dic will work
+  (ispell-set-spellchecker-params)
+  (ispell-hunspell-add-multi-dic "pt_PT,en_US")
+  ;; For saving words to the personal dictionary, don't infer it from
+  ;; the locale, otherwise it would save to ~/.hunspell_de_DE.
+  (setq ispell-personal-dictionary "~/.hunspell_personal"))
+
+;; The personal dictionary file has to exist, otherwise hunspell will
+;; silently not use it.
+(unless (file-exists-p ispell-personal-dictionary)
+  (write-region "" nil ispell-personal-dictionary nil 0))
